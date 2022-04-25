c9b0ff1b947d03ec535d97fc1f676d184ce1bfbb
MissDine <enidnyatichi1212@gmail.com>
2022-04-25 10:05:02 +0300

    Final commit

---
+++ index.js
@@ -0,0 +1,153 @@
+fetch("https://api.currconv.com/api/v7/currencies?&apiKey=0243a45e9b454a58911028adbd33c090")
+. then(res => res. json())
+. then((out) => {
+console.log(typeof out.results);
+const currencies = out.results;
+for (element in currencies){
+  var opt = document.createElement("option");
+  opt.value= currencies[element].id;
+  opt.innerHTML = currencies[element].currencyName;
+const newSelect =document.getElementById("currency");
+newSelect.appendChild(opt);
+}
+})
+. catch(err => { throw err });
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction() {
+  // e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `${total}`;
+  money_plus.innerText = `${income}`;
+  money_minus.innerText = `${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
+
+function changeCurrency(){
+  const id = document.getElementById("currency").value;
+  fetch(`https://api.currconv.com/api/v7/convert?q=USD_${id}&compact=ultra&apiKey=0243a45e9b454a58911028adbd33c090`)
+  .then(response => response.json())
+  .then(data =>{
+    for(con in data){
+      console.log(data[con]);
+      const balanceValue = balance.innerHTML;
+      const newValue = balanceValue * data[con];
+      balance.innerHTML = newValue;
+
+      const incomeValue = money_plus.innerHTML;
+      const newIncome = incomeValue * data[con];
+      money_plus.innerHTML = newIncome;
+      
+      const expenseValue = money_minus.innerHTML;
+      const newExpense = expenseValue * data[con];
+      money_minus.innerHTML = newExpense;
+    }
+  })
+
+}
+
+
+
